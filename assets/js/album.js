0be0b9414a5af911c89253c31d3ef56ff8fedc78
fndomariano <fernando.mar16@gmail.com>
2013-12-18 11:19:10 -0200

    Feita a validação dos formulários de login e cadastro de usuario usando js

---
+++ assets/js/album.js
@@ -0,0 +1,56 @@
+$('.ui.form')
+  .form({
+    name: {
+      identifier  : 'name',
+      rules: [
+        {
+          type   : 'empty',
+          prompt : 'Please enter your NAME'
+        }
+      ]
+    },
+    login: {
+      identifier : 'login',
+      rules: [
+        {
+          type   : 'empty',
+          prompt : 'Please enter a LOGIN'
+        }
+      ]
+    },
+    password: {
+      identifier : 'password',
+      rules: [
+        {
+          type   : 'empty',
+          prompt : 'Please enter a password'
+        },
+        {
+          type   : 'length[6]',
+          prompt : 'Your password must be at least 6 characters'
+        }
+      ]
+    },
+    Login: {
+      identifier : 'login',
+      rules: [
+        {
+          type   : 'empty',
+          prompt : 'Please enter a LOGIN'
+        }
+      ]
+    },
+    Password: {
+      identifier : 'password',
+      rules: [
+        {
+          type   : 'empty',
+          prompt : 'Please enter a password'
+        },
+        {
+          type   : 'length[6]',
+          prompt : 'Your password must be at least 6 characters'
+        }
+      ]
+    }
+  });
